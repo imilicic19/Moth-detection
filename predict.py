dd2c490e81686d8afd95d3be1187684a7bac15a2
imilicic19 <i_milicic@hotmail.com>
2026-04-14 14:14:10 +0200

    predict.py

--- predict.py
+++ predict.py
@@ -1,5 +1,14 @@
 from ultralytics import YOLO
+import torch
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using device: {device}")
 
 model = YOLO("runs/detect/train/weights/best.pt")
-results = model.predict(source="runs/detect/Probe/predict6/142-20230123220000-snapshot.jpg", 
-                        conf=0.2,save=True)
+
+results = model.predict(
+    source="original (2).jpg",
+    conf=0.2,
+    save=True,
+    device=device
+)
